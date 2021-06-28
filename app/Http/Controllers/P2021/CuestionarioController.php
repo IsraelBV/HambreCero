7e7d9782e4e93f21c0caf4cd88618a48943c4904
Israel <israelbalamv@gmail.com>
2021-06-28 14:10:13 -0500

    cambio de emergencia

--- app/Http/Controllers/P2021/CuestionarioController.php
+++ app/Http/Controllers/P2021/CuestionarioController.php
@@ -300,9 +300,15 @@ class CuestionarioController extends Controller
     {   
         // if (Auth::check()) {// cambio por beda electoral
 
+            // $colonias = DB::table('c_colonias')
+            // ->select('c_colonias.*')
+            // ->whereIn('c_colonias.LocalidadId', [1,11,57,58,59,66,68,69,71,76,157,158,214,249,326,330,347])
+            // ->orderBy('c_colonias.Descripcion', 'ASC')
+            // ->get();
             $colonias = DB::table('c_colonias')
             ->select('c_colonias.*')
-            ->whereIn('c_colonias.LocalidadId', [1,11,57,58,59,66,68,69,71,76,157,158,214,249,326,330,347])
+            ->whereIn('c_colonias.LocalidadId', [57,249,326])
+            ->where('c_colonias.status', '=',1)
             ->orderBy('c_colonias.Descripcion', 'ASC')
             ->get();
 
@@ -356,9 +362,11 @@ class CuestionarioController extends Controller
                 'estados'=> C_Estado::all(),
                 'colonias'=> $colonias,
                 // 'colonias'=> C_Colonia::all(),
-                'localidades'=> C_Localidad::findMany([1,11,57,58,59,66,68,69,71,76,157,158,214,249,326,330,347]),   
+                // 'localidades'=> C_Localidad::findMany([1,11,57,58,59,66,68,69,71,76,157,158,214,249,326,330,347]),   
+                'localidades'=> C_Localidad::findMany([57,249,326]),   
                 // 'localidades'=> C_Localidad::findMany([57,249]),   
-                'municipios'=> C_Municipio::findMany([1,2,3,4,5,6,7,8,9,10,11]),
+                // 'municipios'=> C_Municipio::findMany([1,2,3,4,5,6,7,8,9,10,11]),
+                'municipios'=> C_Municipio::findMany([4,5,8]),
                 // 'municipios'=> C_Municipio::findMany([5,4]),
                 'estadosCiviles' => C_EstadoCivil::all(),
                 'estudios'=> C_GradoDeEstudio::all(),
