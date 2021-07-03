61f9cec5b54d516450f299b501042fff87c97f5c
Israel <israelbalamv@gmail.com>
2021-07-03 11:45:39 -0500

    Agregado Cozumel

--- app/Http/Controllers/P2021/CuestionarioController.php
+++ app/Http/Controllers/P2021/CuestionarioController.php
@@ -185,7 +185,7 @@ class CuestionarioController extends Controller
 
             $colonias = DB::table('c_colonias')
             ->select('c_colonias.*')
-            ->whereIn('c_colonias.LocalidadId', [57,249,326,68,71,76,69,79,330,1,11])
+            ->whereIn('c_colonias.LocalidadId', [57,249,326,68,71,76,69,79,330,1,11,66])
             ->where('c_colonias.status', '=',1)
             ->orderBy('c_colonias.Descripcion', 'ASC')
             ->get();
@@ -197,9 +197,9 @@ class CuestionarioController extends Controller
                 'estados'=> C_Estado::all(),
                 'colonias'=> $colonias,
                 // 'colonias'=> C_Colonia::all(),
-                'localidades'=> C_Localidad::findMany([57,249,326,68,71,76,69,79,330,1,11]),   
+                'localidades'=> C_Localidad::findMany([57,249,326,68,71,76,69,79,330,1,11,66]),   
                 // 'localidades'=> C_Localidad::findMany([57,249]),   
-                'municipios'=> C_Municipio::findMany([4,5,8,2,9,10]),
+                'municipios'=> C_Municipio::findMany([4,5,8,2,9,10,1]),
                 // 'municipios'=> C_Municipio::findMany([5,4]),
                 'estadosCiviles' => C_EstadoCivil::all(),
                 'estudios'=> C_GradoDeEstudio::all(),
@@ -312,12 +312,12 @@ class CuestionarioController extends Controller
             } else {
                 $colonias = DB::table('c_colonias')
                 ->select('c_colonias.*')
-                ->whereIn('c_colonias.LocalidadId', [57,249,326,68,71,76,69,79,330,1,11])
+                ->whereIn('c_colonias.LocalidadId', [57,249,326,68,71,76,69,79,330,1,11,66])
                 ->where('c_colonias.status', '=',1)
                 ->orderBy('c_colonias.Descripcion', 'ASC')
                 ->get();
-                $localidades = C_Localidad::findMany([57,249,326,68,71,76,69,79,330,1,11]);
-                $municipios = C_Municipio::findMany([4,5,8,2,9,10]);
+                $localidades = C_Localidad::findMany([57,249,326,68,71,76,69,79,330,1,11,66]);
+                $municipios = C_Municipio::findMany([4,5,8,2,9,10,1]);
             } 
             
 
